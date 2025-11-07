e27486a5260a7a3375349a158946e00576a141d1
Kristjan Kaasik <kristjankk25@gmail.com>
2025-11-07 16:20:31 +0200

    Add pylint to week 2's nhl-reader and fix it's given issues

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,13 +1,18 @@
 class Player:
-    def __init__(self, dict):
-        self.name = dict['name']
-        self.nationality = dict['nationality']
-        self.assists = dict['assists']
-        self.goals = dict['goals']
+    def __init__(self, dictonary):
+        self.name = dictonary['name']
+        self.nationality = dictonary['nationality']
+        self.assists = dictonary['assists']
+        self.goals = dictonary['goals']
         self.points = self.goals + self.assists
-        self.team = dict['team']
-        self.games = dict['games']
-        self.id = dict['id']
-    
+        self.team = dictonary['team']
+        self.games = dictonary['games']
+
+    def name_and_nationality(self):
+        return (self.name, self.nationality)
+
     def __str__(self):
-        return f"{self.name:20} team {self.team:15} {self.goals:2} + {self.assists:2} = {self.points}"
+        return (
+            f"{self.name:20} team {self.team:15} "
+            f"{self.goals:2} + {self.assists:2} = {self.points}"
+        )
--- viikko2/nhl-reader/src/player_reader.py
+++ viikko2/nhl-reader/src/player_reader.py
@@ -3,7 +3,10 @@ from player import Player
 
 class PlayerReader:
     def __init__(self, url):
-        self.players_json = requests.get(url).json()
+        self.players_json = requests.get(url, timeout=10).json()
+
+    def get_player(self, player):
+        return Player(player)
 
     def get_players(self):
         return [Player(player) for player in self.players_json]

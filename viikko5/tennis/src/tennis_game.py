2b819f4ce0d254e6013f74e1b1ab276ba5000302
Kristjan Kaasik <kristjankk25@gmail.com>
2025-11-26 20:35:18 +0200

    Refactor tennis class

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,12 +1,26 @@
 class TennisGame:
-    LOVE = 0
-    FIFTEEN = 1
-    THIRTY = 2
-    FORTY = 3
-
     LEADER_PLAYER1 = 1
     LEADER_PLAYER2 = -1
     WINNER_PLAYER1 = 2
+
+    END_CLOSE = 4
+
+    ADVANTAGE = "Advantage"
+    WIN_FOR = "Win for"
+    DEUCE = "Deuce"
+
+    POINTS_EVEN = {
+        0: "Love-All",
+        1: "Fifteen-All",
+        2: "Thirty-All"
+    }
+
+    POINTS = {
+        0: "Love",
+        1: "Fifteen",
+        2: "Thirty",
+        3: "Forty"
+    }
 
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
@@ -16,15 +30,15 @@ class TennisGame:
 
     def won_point(self, player_name):
         if player_name == self.player1_name:
-            self.m_score1 = self.m_score1 + 1
+            self.m_score1 += 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.m_score2 += 1
 
     def get_score(self):
         if self.m_score1 == self.m_score2:
             score = self._score_even(self.m_score1)
 
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
+        elif self.m_score1 >= self.END_CLOSE or self.m_score2 >= self.END_CLOSE:
             score = self._score_lead(self.m_score1, self.m_score2)
         
         else:
@@ -33,48 +47,20 @@ class TennisGame:
         return score
     
     def _score_even(self, score):
-        if score == self.LOVE:
-            score = "Love-All"
-        elif score == self.FIFTEEN:
-            score = "Fifteen-All"
-        elif score == self.THIRTY:
-            score = "Thirty-All"
-        else:
-            score = "Deuce"
-
-        return score
+        return self.POINTS_EVEN.get(score, self.DEUCE)
 
     def _score_lead(self, score1, score2):
         difference = score1 - score2
         if difference == self.LEADER_PLAYER1:
-            score = "Advantage player1"
+            score = f"{self.ADVANTAGE} {self.player1_name}"
         elif difference == self.LEADER_PLAYER2:
-            score = "Advantage player2"
+            score = f"{self.ADVANTAGE} {self.player2_name}"
         elif difference >= self.WINNER_PLAYER1:
-            score = "Win for player1"
+            score = f"{self.WIN_FOR} {self.player1_name}"
         else:
-            score = "Win for player2"
+            score = f"{self.WIN_FOR} {self.player2_name}"
 
         return score
 
     def _score_normal(self, score1, score2):
-        score = ""
-        temp_score = 0
-
-        for i in range(1, 3):
-            if i == 1:
-                temp_score = score1
-            else:
-                score = score + "-"
-                temp_score = score2
-
-            if temp_score == self.LOVE:
-                score = score + "Love"
-            elif temp_score == self.FIFTEEN:
-                score = score + "Fifteen"
-            elif temp_score == self.THIRTY:
-                score = score + "Thirty"
-            elif temp_score == self.FORTY:
-                score = score + "Forty"
-
-        return score
+        return f"{self.POINTS[score1]}-{self.POINTS[score2]}"

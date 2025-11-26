7296c7059b43517a48614e32381bf61b66f2f77a
Kristjan Kaasik <kristjankk25@gmail.com>
2025-11-26 20:14:07 +0200

    Refactor tennis normal scores

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -2,10 +2,11 @@ class TennisGame:
     LOVE = 0
     FIFTEEN = 1
     THIRTY = 2
+    FORTY = 3
 
     LEADER_PLAYER1 = 1
     LEADER_PLAYER2 = -1
-    WINNDER_PLAYER1 = 2
+    WINNER_PLAYER1 = 2
 
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
@@ -20,31 +21,14 @@ class TennisGame:
             self.m_score2 = self.m_score2 + 1
 
     def get_score(self):
-        score = ""
-        temp_score = 0
-
         if self.m_score1 == self.m_score2:
-            return self._score_even(self.m_score1)
+            score = self._score_even(self.m_score1)
 
         elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            return self._score_lead(self.m_score1, self.m_score2)
-
+            score = self._score_lead(self.m_score1, self.m_score2)
+        
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
-
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+            score = self._score_normal(self.m_score1, self.m_score2)
 
         return score
     
@@ -66,9 +50,31 @@ class TennisGame:
             score = "Advantage player1"
         elif difference == self.LEADER_PLAYER2:
             score = "Advantage player2"
-        elif difference >= self.WINNDER_PLAYER1:
+        elif difference >= self.WINNER_PLAYER1:
             score = "Win for player1"
         else:
             score = "Win for player2"
+
+        return score
+
+    def _score_normal(self, score1, score2):
+        score = ""
+        temp_score = 0
+
+        for i in range(1, 3):
+            if i == 1:
+                temp_score = score1
+            else:
+                score = score + "-"
+                temp_score = score2
+
+            if temp_score == self.LOVE:
+                score = score + "Love"
+            elif temp_score == self.FIFTEEN:
+                score = score + "Fifteen"
+            elif temp_score == self.THIRTY:
+                score = score + "Thirty"
+            elif temp_score == self.FORTY:
+                score = score + "Forty"
 
         return score

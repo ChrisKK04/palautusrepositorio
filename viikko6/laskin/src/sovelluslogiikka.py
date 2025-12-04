b92acff1c0bee33f0f136ddb7f658c1d2e6ab7f0
Kristjan Kaasik <kristjankk25@gmail.com>
2025-12-04 19:58:42 +0200

    Week 6 exercise 2 + bonus exercise

--- viikko6/laskin/src/sovelluslogiikka.py
+++ viikko6/laskin/src/sovelluslogiikka.py
@@ -1,12 +1,19 @@
 class Sovelluslogiikka:
     def __init__(self, arvo=0):
         self._arvo = arvo
+        self._edelliset_arvot = [0]
 
     def aseta_arvo(self, arvo):
         self._arvo = arvo
 
+    def aseta_edelliset_arvot(self, arvot):
+        self._edelliset_arvot = arvot
+
     def arvo(self):
         return self._arvo
+    
+    def edelliset_arvot(self):
+        return self._edelliset_arvot
 
 class Summa:
     def __init__(self, sovelluslogiikka, syote):
@@ -16,6 +23,10 @@ class Summa:
     def suorita(self):
         nykyinen = self._sovelluslogiikka.arvo()
         uusi = nykyinen + self._syote()
+
+        arvot = self._sovelluslogiikka.edelliset_arvot()
+        arvot.append(uusi)
+        self._sovelluslogiikka.aseta_edelliset_arvot(arvot)
 
         self._sovelluslogiikka.aseta_arvo(uusi)
 
@@ -28,6 +39,10 @@ class Erotus:
         nykyinen = self._sovelluslogiikka.arvo()
         uusi = nykyinen - self._syote()
 
+        arvot = self._sovelluslogiikka.edelliset_arvot()
+        arvot.append(uusi)
+        self._sovelluslogiikka.aseta_edelliset_arvot(arvot)
+
         self._sovelluslogiikka.aseta_arvo(uusi)
 
 class Nollaus:
@@ -36,8 +51,17 @@ class Nollaus:
         self._syote = syote
     
     def suorita(self):
+        self._sovelluslogiikka.aseta_edelliset_arvot([0])
         self._sovelluslogiikka.aseta_arvo(0)
 
 class Kumoa:
     def __init__(self, sovelluslogiikka, syote):
-        pass
+        self._sovelluslogiikka = sovelluslogiikka
+        self._syote = syote
+
+    def suorita(self):
+        arvot = self._sovelluslogiikka.edelliset_arvot()
+        if len(arvot) > 1:
+            arvot.pop()
+            self._sovelluslogiikka.aseta_edelliset_arvot(arvot)
+            self._sovelluslogiikka.aseta_arvo(self._sovelluslogiikka.edelliset_arvot()[-1])

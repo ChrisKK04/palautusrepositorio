77b68ee5ae2e377c58369dfa4d1e12e78d7511f8
Kristjan Kaasik <kristjankk25@gmail.com>
2025-12-05 16:01:04 +0200

    Week 6 exercise 6

--- viikko6/query-language/src/query_builder.py
+++ viikko6/query-language/src/query_builder.py
@@ -1,4 +1,4 @@
-from matchers import And, HasAtLeast, PlaysIn, All, HasFewerThan
+from matchers import And, HasAtLeast, PlaysIn, All, HasFewerThan, Or
 
 EMPTY = 0
 
@@ -19,8 +19,12 @@ class QueryBuilder:
     def has_fewer_than(self, value, attribute):
         new_query = self.query + [HasFewerThan(value, attribute)]
         return QueryBuilder(new_query)
+    
+    def one_of(self, *queries):
+        matchers = [query.build() for query in queries]
+        return QueryBuilder([Or(*matchers)])
 
     def build(self):
         if len(self.query) == EMPTY:
-            return All(all)
+            return All()
         return And(*self.query)
